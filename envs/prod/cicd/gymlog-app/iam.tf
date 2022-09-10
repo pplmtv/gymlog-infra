9b4fb5d72896aa69ceb96fd648913dd8da8bf9a5
pplmtv <pplmtv@gmail.com>
2022-09-11 00:16:59 +0900

    iam.tf fixed

--- envs/prod/cicd/gymlog-app/iam.tf
+++ envs/prod/cicd/gymlog-app/iam.tf
@@ -54,7 +54,7 @@ resource "aws_iam_role_policy" "s3" {
           "Action" : [
             "s3:GetObject"
           ],
-          "Resource" : "arn:aws:s3:::pplmtv-tfstate/${local.system_name}/${local.env_name}/cicd/app_${local.service_name}_*.tfstate"
+          "Resource" : "arn:aws:s3:::pplmtv-tfstate/${local.system_name}/${local.env_name}/cicd/${local.service_name}_*.tfstate"
         },
         {
           "Effect" : "Allow",
